dab313024488d4a952a435ed0de80bb2dfec6f0b
cherifb16 <cherifbobbyray@gmail.com>
2019-10-01 09:02:29 +0000

    step 12

--- spec/models/task.spec.rb
+++ spec/models/task.spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 RSpec.describe Task, type: :model do
  it "Validation does not pass if the name is empty" do
    task = Task.new(name: '', details: 'Failure test')
@@ -12,4 +13,5 @@ RSpec.describe Task, type: :model do
    task = Task.new(name: '', details: '')
    expect(task).not_to be_valid
  end
+
 end
